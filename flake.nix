09a8ba7cf7f6b7dd0d83b3df122c38933495b101
Percy Timon <percygt.dev@gmail.com>
2026-03-31 11:52:47 +0800

    refactor(lib): modularize file collection and tree importing
    - Split monolithic import-tree into collectNixFiles and importTree.
    - Improve path filtering logic for underscore-prefixed directories.
    - Use nixpkgs-lib input for a lighter footprint.

--- flake.nix
+++ flake.nix
@@ -1,18 +1,29 @@
 {
   description = "myLibs";
-
   inputs.nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
-
   outputs =
     { nixpkgs-lib, ... }:
     let
       inherit (nixpkgs-lib) lib;
     in
-    {
-      import-tree = path: {
-        imports = lib.fileset.toList (
-          lib.fileset.fileFilter (file: file.hasExt "nix" && !(lib.hasPrefix "_" file.name)) path
-        );
+    rec {
+      collectNixFiles =
+        path:
+        let
+          nixFiles = lib.fileset.toList (
+            lib.fileset.fileFilter (file: file.hasExt "nix" && !(lib.hasPrefix "_" file.name)) path
+          );
+          pathStr = toString path;
+          hasUnderscoreDir =
+            file:
+            lib.any (lib.hasPrefix "_") (
+              lib.splitString "/" (lib.removePrefix (pathStr + "/") (toString file))
+            );
+        in
+        builtins.filter (file: !(hasUnderscoreDir file)) nixFiles;
+
+      importTree = path: {
+        imports = collectNixFiles path;
       };
     };
 }
